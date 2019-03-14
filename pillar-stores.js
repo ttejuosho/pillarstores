ac922161951904b3914a303fedced0517afc9115
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2019-03-13 20:25:01 -0500

    Open for Business

---
+++ pillar-stores.js
@@ -0,0 +1,11 @@
+var action = process.argv[2];
+var item = process.argv[3];
+var quantity = process.argv[4];
+
+var cart = {};
+
+switch (action){
+    case "buy":
+        getCart();
+        break;
+}
